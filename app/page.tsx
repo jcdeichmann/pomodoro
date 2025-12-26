c85ca6582d147274afd4349c5b4ada0ac91951ea
Joshua Deichmann <jcdeichmann@gmail.com>
2025-12-26 11:04:47 -0500

    Test: Change background to orange

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Timer } from '@/components/Timer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-950">
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-orange-600 to-orange-800">
       <Timer />
     </main>
   );
